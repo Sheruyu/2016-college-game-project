ee543e498db17f2e0b974fe7dd7da755f458a456
Sheruyu <41010644+Sheruyu@users.noreply.github.com>
2018-07-17 23:25:22 -0500

    College group project game
    Game where you control monsters to eat a certain colored berry.

---
+++ CollisionScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+
+	public VariableList target1;
+
+	void Start () {
+		target1 = GameObject.Find ("Main Camera").GetComponent<VariableList>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D co) {
+		
+		if (co.name == "centerObject") {
+			Destroy (gameObject);
+		}
+		else if (co.name == "greenmonster_0") {
+			if(gameObject.name.Equals ("greenFruit(Clone)")){
+			   target1.counter++; //increment score
+			   target1.animationNum = 3;
+			   target1.animationTime = Time.time + .5F;
+			}
+			else{
+				target1.lives--; //decrement lives
+				target1.animationNum = 4;
+				target1.animationTime = Time.time + .5F;
+			}
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ VariableList.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariableList : MonoBehaviour {
+	
+	public int counter = 0;
+	public float speed = 2.0f; //fruit velocity
+	public int lives = 3;
+	public bool gameOver = false;
+	public bool animationState = false; //also pause on/off boolean
+	public int animationNum = 0; //determines which animation is played
+	public float animationTime = 1F; //control time between animations. No need to update on start
+	public bool settingsOpened = false;
+
+
+	void Start(){
+		DontDestroyOnLoad (gameObject); //same camera is used for every scene, cameras in other scenes are deleted
+	}
+	
+	void FixedUpdate(){
+		if (animationState == false) {
+			speed *= 1.0002f;
+		}
+		if (lives <= 0) {
+			gameOver = true;
+		}
+		
+	}
+}
